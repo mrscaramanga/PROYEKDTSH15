fe890d2dbad79fd65aefa6950ed356f741123a33
Sakakibara <63957239+mrscaramanga@users.noreply.github.com>
2020-04-28 18:24:26 +0700

    Update coding.js

--- coding.js
+++ coding.js
@@ -2,13 +2,7 @@
 
 window.onload = function() {
 
-var current,
-    screen,
-    output,
-    limit,
-    zero,
-    period,
-    operator;
+var current,screen,output,limit,zero,period,operator;
     
     screen = document.getElementById("result");
 
